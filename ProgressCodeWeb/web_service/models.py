a06bf20d56ec9f7e8023df12fb1f34ee50caf188
icunhacorrea <iago.cc@outlook.com>
2016-11-21 03:01:44 -0200

    commit_milagroso

--- ProgressCodeWeb/web_service/models.py
+++ ProgressCodeWeb/web_service/models.py
@@ -126,11 +126,12 @@ class Atividade(models.Model):
 
 class Feedback(models.Model):
     status = models.IntegerField()
-    timestamp = models. DateTimeField(auto_now=False)
+    timestamp = models. DateTimeField(auto_now=True)
     dir_audio = models.FileField(upload_to='sounds/')
     tutor = models.ForeignKey(Tutor, on_delete=models.CASCADE)
     inscrito = models.ForeignKey(Inscrito, on_delete=models.CASCADE)
     atividade = models.ForeignKey(Atividade, on_delete=models.CASCADE)
+    observacao = models.TextField()
 
     class Meta:
         db_table  =  'feedback'
